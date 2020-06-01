a01efbe40512d6b1056a1fac3d38933dfb0ef18e
Arthur M. <milon.arthur@gmail.com>
2020-06-01 12:15:52 +0200

    Ext Img

--- src/graphics/shapes/ShapeVisitor.java
+++ src/graphics/shapes/ShapeVisitor.java
@@ -6,5 +6,6 @@ public interface ShapeVisitor {
 	public void visitCircle(SCircle circ);
 	public void visitText(SText text);
 	public void visitCollection(SCollection col);
+	//Ext Img
 	public void visitImage(SImage sImage);
 }
